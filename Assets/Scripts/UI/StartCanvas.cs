2ddc065ccc68ec2b94792bfde84daab4fda9c43f
raulsg3 <raulseguragaleote@gmail.com>
2017-09-17 18:08:16 +0200

    Start scene

---
+++ Assets/Scripts/UI/StartCanvas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartCanvas : MonoBehaviour
+{
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("AsteroidRain");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+}
